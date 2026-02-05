ed1ebdf58e784fa32b0c59d1dfc7b01a76dd6077
karimgaza <karim12.gazzah@gmail.com>
2026-02-05 23:16:04 +0100

    base de donne et crud consultation

--- src/Repository/ConsultationRepository.php
+++ src/Repository/ConsultationRepository.php
@@ -8,6 +8,11 @@ use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<Consultation>
+ *
+ * @method Consultation|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Consultation|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Consultation[]    findAll()
+ * @method Consultation[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class ConsultationRepository extends ServiceEntityRepository
 {
@@ -16,28 +21,28 @@ class ConsultationRepository extends ServiceEntityRepository
         parent::__construct($registry, Consultation::class);
     }
 
-    //    /**
-    //     * @return Consultation[] Returns an array of Consultation objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('c.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+//    /**
+//     * @return Consultation[] Returns an array of Consultation objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('c.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
 
-    //    public function findOneBySomeField($value): ?Consultation
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+//    public function findOneBySomeField($value): ?Consultation
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
 }
